ff21985c525054cc58d5b574a745af0e4062f0c0
草太郎＿９３６ <kusatarou.936@gmail.com>
2026-04-03 14:29:53 +0900

    Update detectTechStack.ts

--- lib/analyzer/detectTechStack.ts
+++ lib/analyzer/detectTechStack.ts
@@ -1,1 +1,17 @@
+import { FileInput } from "../types/FileInput";
 
+export function detectTechStack(files: FileInput[]): string[] {
+  const tech: Set<string> = new Set();
+
+  for (const file of files) {
+    const c = file.content;
+
+    if (c.includes("express")) tech.add("Express");
+    if (c.includes("redis")) tech.add("Redis");
+    if (c.includes("prisma")) tech.add("Prisma");
+    if (c.includes("next")) tech.add("Next.js");
+    if (c.includes("nest")) tech.add("NestJS");
+  }
+
+  return [...tech];
+}
