9f23891357885e1b5f260209ae2b9b30bafee9c9
草太郎＿９３６ <kusatarou.936@gmail.com>
2026-04-03 14:32:45 +0900

    Update buildExcerptText.ts

--- lib/chunker/buildExcerptText.ts
+++ lib/chunker/buildExcerptText.ts
@@ -1,1 +1,9 @@
+import { ExcerptItem } from "../types/ExcerptItem";
 
+export function buildExcerptText(ex: ExcerptItem): string {
+  return `## ${ex.label}
+理由: ${ex.reason}
+----------------------------------------
+${ex.content}
+`;
+}
