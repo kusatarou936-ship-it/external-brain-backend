a7112ae1cacd281f7fe6243bcdcd49e18b0ec939
草太郎＿９３６ <kusatarou.936@gmail.com>
2026-04-03 14:29:37 +0900

    Update analyzeImports.ts

--- lib/analyzer/analyzeImports.ts
+++ lib/analyzer/analyzeImports.ts
@@ -1,1 +1,12 @@
+import { FileInput } from "../types/FileInput";
 
+export function analyzeImports(files: FileInput[]): Record<string, string[]> {
+  const result: Record<string, string[]> = {};
+
+  for (const file of files) {
+    const imports = [...file.content.matchAll(/from\s+["'](.+?)["']/g)];
+    result[file.path] = imports.map((m) => m[1]);
+  }
+
+  return result;
+}
